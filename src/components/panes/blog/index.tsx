244452f7ba0be65d84d58ae28dc4f6d70ae4f650
Ryne Burden <ryneburden@protonmail.com>
2025-01-29 22:40:22 -0500

    refactor home page and remove blog page and components

--- src/components/panes/blog/index.tsx
+++ src/components/panes/blog/index.tsx
@@ -3,12 +3,25 @@ import type { BlogPost } from "@hideoutTypes/blog";
 
 import styles from "./blog.module.scss";
 
-// const posts = Object.values(
-//   import.meta.glob<MarkdownInstance<BlogPost>>("@content/blog/*.md", {
-//     eager: true,
-//   }),
-// ).sort((a, b) => (a.frontmatter.date > b.frontmatter.date ? 1 : 0));
+interface IBlog {
+  posts: MarkdownInstance<BlogPost>[];
+}
 
-export const Blog = () => {
-  return <>bleg</>;
+export const Blog = ({ posts }: IBlog) => {
+  const latestPost = posts[posts.length - 1];
+  return (
+    <div>
+      <div className={styles.header}>Latest Blog Post</div>
+      <div className={styles.postContainer}>
+        <h2>{latestPost.frontmatter.title}</h2>
+        <span>{latestPost.frontmatter.description}</span>
+        <span>Reading time calculation</span>
+        <span>Read More button</span>
+        <span>Arrows on the side to cycle through articles</span>
+        <span>
+          Expand button that will fit content height and transition smoothly
+        </span>
+      </div>
+    </div>
+  );
 };
--- src/types/blog.ts
+++ src/types/blog.ts
@@ -1,5 +1,5 @@
 export interface BlogPost {
   title: string;
-  date: Date;
+  date: string;
   description?: string;
 }

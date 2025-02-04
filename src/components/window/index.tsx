04b17d9b86d19d55c21eb724d24f8cda7099190f
Ryne Burden <ryneburden@protonmail.com>
2025-02-04 00:11:03 -0500

    add basic portfolio page and add dynamic height window

--- src/components/window/index.tsx
+++ src/components/window/index.tsx
@@ -3,9 +3,20 @@ import type { ReactNode } from "react";
 import styles from "./window.module.scss";
 
 interface IWindow {
+  dynamicHeight?: boolean;
   children: ReactNode;
 }
 
-export const Window = ({ children }: IWindow) => {
-  return <div className={styles.windowContainer}>{children}</div>;
+export const Window = ({ dynamicHeight, children }: IWindow) => {
+  return (
+    <div
+      className={
+        dynamicHeight
+          ? styles.dynamicHeightWindowContainer
+          : styles.windowContainer
+      }
+    >
+      {children}
+    </div>
+  );
 };

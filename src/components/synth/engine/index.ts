f86c7e96e5c0795d5664baf1897482c6196f3936
Ryne Burden <ryneburden@protonmail.com>
2025-10-08 23:27:17 -0400

    move synth logic to audio engine singleton

---
+++ src/components/synth/engine/index.ts
@@ -0,0 +1,199 @@
+import { GAIN_MAX } from "@constants/synth";
+import type {
+  HideoutWaveforms,
+  SynthDelay,
+  SynthFilter,
+  SynthReverb,
+} from "@hideoutTypes/synth";
+import { synthParamsStore } from "@stores/synth";
+
+class SynthEngine {
+  /* ----- Class Members ----- */
+  private audio = new AudioContext();
+
+  // gain nodes
+  private gain = this.audio.createGain();
+  private gain2 = this.audio.createGain();
+  private reverbGain = this.audio.createGain();
+
+  // oscillators
+  private oscillator = this.audio.createOscillator();
+  private oscillator2 = this.audio.createOscillator();
+
+  // space
+  private delay = this.audio.createDelay();
+  private reverb = this.audio.createConvolver();
+
+  // filter
+  private filter = this.audio.createBiquadFilter();
+
+  // synth params
+  private params = synthParamsStore.get();
+
+  /* ----- Methods ----- */
+  public constructor() {
+    synthParamsStore.subscribe((value, oldValue, key) => {
+      this.params = value;
+      switch (key) {
+        case "reverb":
+          this.updateReverb(value.reverb);
+          break;
+        case "delay":
+          this.updateDelay(value.delay);
+          break;
+        case "filter":
+          this.updateFilter(value.filter);
+          break;
+        case "waveform":
+          this.updateOscillatorWaveform(value.waveform);
+          break;
+        case "secondOscOn":
+          this.toggleSecondOscillator();
+          break;
+      }
+    });
+  }
+
+  public init() {
+    this.oscillator.connect(this.filter);
+    this.oscillator2.connect(this.gain2);
+    this.filter.connect(this.gain);
+    this.gain.connect(this.audio.destination);
+
+    this.gain.gain.value = GAIN_MAX;
+    this.gain2.gain.value = GAIN_MAX - 0.1;
+  }
+
+  public cleanup() {
+    this.oscillator.stop();
+    this.oscillator2.stop();
+    this.audio.close();
+  }
+
+  public playOscillator(frequency: number): void {
+    try {
+      this.oscillator.start();
+      this.oscillator2.start();
+    } catch {}
+
+    this.gain.gain.cancelScheduledValues(this.audio.currentTime);
+    this.filter.frequency.cancelScheduledValues(this.audio.currentTime);
+    this.gain.gain.setValueAtTime(0, this.audio.currentTime);
+
+    this.oscillator.frequency.setValueAtTime(
+      frequency * this.params.octave,
+      this.audio.currentTime,
+    );
+    this.oscillator2.frequency.setValueAtTime(
+      frequency * this.params.octave,
+      this.audio.currentTime,
+    ); // detune setting??
+
+    const attackTime =
+      this.audio.currentTime + this.params.envelope.attack / 100;
+    const decayTime = attackTime + this.params.envelope.decay / 100;
+
+    this.gain.gain.linearRampToValueAtTime(this.params.gain, attackTime);
+    if (this.params.filter.envelopeLink) {
+      // js is dumb, had to parse these as floats
+      const maxFilterFrequency =
+        parseFloat(`${this.params.filter.frequency}`) +
+        parseFloat(`${this.params.filter.frequency}`) *
+          (this.params.envelope.decay / 100);
+
+      this.filter.frequency.linearRampToValueAtTime(
+        maxFilterFrequency,
+        attackTime,
+      );
+    }
+    // apply decay into sustain
+    this.gain.gain.linearRampToValueAtTime(
+      this.params.gain * (this.params.envelope.sustain / 100),
+      decayTime,
+    );
+
+    if (this.params.filter.envelopeLink)
+      this.filter.frequency.linearRampToValueAtTime(
+        this.params.filter.frequency * (this.params.envelope.sustain / 100),
+        decayTime,
+      );
+  }
+
+  public stopOscillator(): void {
+    this.gain.gain.cancelScheduledValues(this.audio.currentTime);
+    this.filter.frequency.cancelScheduledValues(this.audio.currentTime);
+
+    this.gain.gain.linearRampToValueAtTime(
+      0,
+      this.audio.currentTime + this.params.envelope.release / 100,
+    );
+
+    if (this.params.filter.envelopeLink) {
+      const filterEndFreq =
+        parseFloat(`${this.params.filter.frequency}`) -
+        parseFloat(`${this.params.filter.frequency}`) *
+          (this.params.envelope.release / 100);
+
+      this.filter.frequency.linearRampToValueAtTime(
+        filterEndFreq,
+        this.audio.currentTime + this.params.envelope.release / 100,
+      );
+    }
+  }
+
+  private updateFilter(update: SynthFilter) {
+    this.filter.frequency.value = update.frequency;
+    this.filter.type = update.type;
+    this.filter.Q.value = update.q;
+  }
+
+  private updateDelay(update: SynthDelay) {
+    if (!!update.time) {
+      this.delay.delayTime.value = update.time / 1000;
+
+      this.gain.connect(this.delay);
+      this.delay.connect(this.audio.destination);
+    } else {
+      // turn off here
+    }
+  }
+
+  private updateReverb(update: SynthReverb) {
+    if (update.decay !== null) {
+      this.reverbGain.gain.value = update.mix;
+      this.reverb.buffer = this.createImpulseResponse(update.decay);
+
+      this.gain.connect(this.reverb);
+      this.reverb.connect(this.reverbGain);
+      this.reverbGain.connect(this.audio.destination);
+    } else {
+      // turn off here
+    }
+  }
+
+  private updateOscillatorWaveform(update: HideoutWaveforms) {
+    this.oscillator2.type = "sawtooth";
+    this.oscillator.type = update as OscillatorType;
+  }
+
+  private toggleSecondOscillator() {
+    this.gain2.connect(this.filter);
+  }
+
+  private createImpulseResponse(decayTimeMs: number) {
+    const sampleRate = this.audio.sampleRate;
+    const length = 2 * sampleRate; // 2 seconds
+    const impulse = this.audio.createBuffer(2, length, sampleRate);
+
+    for (let channel = 0; channel < 2; channel++) {
+      const channelData = impulse.getChannelData(channel);
+      for (let i = 0; i < length; i++) {
+        const decay = Math.exp(-i / (sampleRate * decayTimeMs));
+        channelData[i] = (Math.random() * 2 - 1) * decay;
+      }
+    }
+    return impulse;
+  }
+}
+
+export const synthEngine = new SynthEngine();
--- src/components/synth/oscillator/index.tsx
+++ src/components/synth/oscillator/index.tsx
@@ -5,25 +5,14 @@ import { synthParamsStore } from "@stores/synth";
 import {
   actionKeys,
   chromaticKeys,
-  GAIN_MAX,
   octaveOptions,
   waveforms,
 } from "@constants/synth";
 import type { ActionDirection, ActionKey } from "@hideoutTypes/synth";
+import { synthEngine } from "../engine";
 
 export const SynthOscillator = () => {
   const params = useStore(synthParamsStore);
-
-  const audioRef = useRef<AudioContext | null>(null);
-  const gainNodeRef = useRef<GainNode | null>(null);
-  const osc2gainNodeRef = useRef<GainNode | null>(null);
-  const oscRef = useRef<OscillatorNode | null>(null);
-  const osc2Ref = useRef<OscillatorNode | null>(null);
-  const delayRef = useRef<DelayNode | null>(null);
-  const delayFeedbackRef = useRef<GainNode | null>(null);
-  const convolverRef = useRef<ConvolverNode | null>(null);
-  const reverbGainRef = useRef<GainNode | null>(null);
-  const filterRef = useRef<BiquadFilterNode | null>(null);
 
   const pressedKey = useRef<string | null>(null);
 
@@ -57,160 +46,30 @@ export const SynthOscillator = () => {
     }
   };
 
-  const handleChromaticKeyDown = (frequency: number, time: number) => {
-    gainNodeRef.current!.gain.cancelScheduledValues(time);
-    filterRef.current!.frequency.cancelScheduledValues(time);
-    gainNodeRef.current!.gain.setValueAtTime(0, time);
-
-    oscRef.current!.frequency.setValueAtTime(frequency * params.octave, time);
-    osc2Ref.current!.frequency.setValueAtTime(frequency * params.octave, time);
-    // if (params.filter.envelopeLink)
-    //   filterRef.current!.frequency.setValueAtTime(
-    //     params.filter.frequency,
-    //     time,
-    //   );
-
-    const attackTime = time + params.envelope.attack / 100;
-    const decayTime = attackTime + params.envelope.decay / 100;
-
-    gainNodeRef.current!.gain.linearRampToValueAtTime(params.gain, attackTime);
-    if (params.filter.envelopeLink) {
-      // js is dumb, had to parse these as floats
-      const maxFilterFrequency =
-        parseFloat(`${params.filter.frequency}`) +
-        parseFloat(`${params.filter.frequency}`) *
-          (params.envelope.decay / 100);
-
-      filterRef.current!.frequency.linearRampToValueAtTime(
-        maxFilterFrequency,
-        attackTime,
-      );
-    }
-    // apply decay into sustain
-    gainNodeRef.current!.gain.linearRampToValueAtTime(
-      params.gain * (params.envelope.sustain / 100),
-      decayTime,
-    );
-
-    if (params.filter.envelopeLink)
-      filterRef.current!.frequency.linearRampToValueAtTime(
-        params.filter.frequency * (params.envelope.sustain / 100),
-        decayTime,
-      );
-  };
-
-  const handleChromaticKeyUp = (time: number) => {
-    gainNodeRef.current!.gain.cancelScheduledValues(time);
-    filterRef.current!.frequency.cancelScheduledValues(time);
-
-    gainNodeRef.current!.gain.linearRampToValueAtTime(
-      0,
-      time + params.envelope.release / 100,
-    );
-
-    if (params.filter.envelopeLink) {
-      const filterEndFreq =
-        parseFloat(`${params.filter.frequency}`) -
-        parseFloat(`${params.filter.frequency}`) *
-          (params.envelope.release / 100);
-
-      filterRef.current!.frequency.linearRampToValueAtTime(
-        filterEndFreq,
-        time + params.envelope.release / 100,
-      );
+  const keyupListener = (e: KeyboardEvent) => {
+    if (pressedKey.current === e.code) {
+      e.preventDefault();
+      synthEngine.stopOscillator();
+      pressedKey.current = null;
     }
   };
-
-  const createImpulseResponse = (audioContext: AudioContext) => {
-    const sampleRate = audioContext.sampleRate;
-    const length = 2 * sampleRate; // 2 seconds
-    const impulse = audioContext.createBuffer(2, length, sampleRate);
-
-    for (let channel = 0; channel < 2; channel++) {
-      const channelData = impulse.getChannelData(channel);
-      for (let i = 0; i < length; i++) {
-        // Create a decaying noise for natural reverb sound
-        const decay = Math.exp(-i / (sampleRate * params.reverb.decay!)); // Adjust decay time here
-        channelData[i] = (Math.random() * 2 - 1) * decay;
+  const keydownListener = (e: KeyboardEvent) => {
+    const chromaticKey = chromaticKeys[e.code];
+    const actionKey = actionKeys[e.code];
+    if (chromaticKey) {
+      if (pressedKey.current !== e.code) {
+        pressedKey.current = e.code;
+        const freq = chromaticKey.baseFrequency * params.octave; // can I move this to the synth engine?
+        synthEngine.playOscillator(chromaticKey.baseFrequency);
       }
+    } else if (actionKey) {
+      e.preventDefault();
+      handleActionKeys(actionKey);
     }
-    return impulse;
   };
 
   useEffect(() => {
-    audioRef.current = new window.AudioContext();
-    gainNodeRef.current = audioRef.current.createGain();
-    osc2gainNodeRef.current = audioRef.current.createGain();
-    oscRef.current = audioRef.current.createOscillator();
-    osc2Ref.current = audioRef.current.createOscillator();
-    delayRef.current = audioRef.current.createDelay();
-    delayFeedbackRef.current = audioRef.current.createGain();
-    convolverRef.current = audioRef.current.createConvolver();
-    reverbGainRef.current = audioRef.current.createGain();
-    filterRef.current = audioRef.current.createBiquadFilter();
-
-    gainNodeRef.current.gain.value = 0;
-    osc2gainNodeRef.current.gain.value = GAIN_MAX - 0.1;
-    filterRef.current.frequency.value = params.filter.frequency;
-    filterRef.current.type = params.filter.type;
-    filterRef.current.Q.value = params.filter.q;
-
-    oscRef.current.connect(filterRef.current!);
-    osc2Ref.current.connect(osc2gainNodeRef.current);
-    filterRef.current.connect(gainNodeRef.current);
-    gainNodeRef.current.connect(audioRef.current.destination);
-
-    if (params.secondOscOn) osc2gainNodeRef.current.connect(filterRef.current!);
-    if (params.delay.time !== null) {
-      // I don't like the conditionals here. It makes the signal flow connections unclear
-      delayFeedbackRef.current.gain.value = params.delay.feedback;
-      delayRef.current.delayTime.value = params.delay.time / 1000;
-
-      gainNodeRef.current.connect(delayRef.current);
-      delayRef.current.connect(delayFeedbackRef.current);
-      delayFeedbackRef.current.connect(delayRef.current); // Create feedback loop
-      delayRef.current.connect(audioRef.current.destination);
-    }
-
-    // I don't like the conditionals here. It makes the signal flow connections unclear
-    if (params.reverb.decay !== null) {
-      reverbGainRef.current.gain.value = params.reverb.mix;
-      convolverRef.current.buffer = createImpulseResponse(audioRef.current);
-
-      gainNodeRef.current.connect(convolverRef.current);
-      convolverRef.current.connect(reverbGainRef.current);
-      reverbGainRef.current.connect(audioRef.current.destination);
-    }
-
-    oscRef.current.type = params.waveform as OscillatorType;
-    osc2Ref.current.type = "sawtooth";
-
-    const keyupListener = (e: KeyboardEvent) => {
-      if (pressedKey.current === e.code) {
-        e.preventDefault();
-        handleChromaticKeyUp(audioRef.current!.currentTime);
-        pressedKey.current = null;
-      }
-    };
-    const keydownListener = (e: KeyboardEvent) => {
-      const chromaticKey = chromaticKeys[e.code];
-      const actionKey = actionKeys[e.code];
-      try {
-        oscRef.current!.start();
-        osc2Ref.current!.start();
-      } catch {}
-      if (chromaticKey) {
-        if (pressedKey.current !== e.code) {
-          pressedKey.current = e.code;
-          const time = audioRef.current!.currentTime;
-          const freq = chromaticKey.baseFrequency * params.octave;
-          handleChromaticKeyDown(freq, time);
-        }
-      } else if (actionKey) {
-        e.preventDefault();
-        handleActionKeys(actionKey);
-      }
-    };
+    synthEngine.init();
 
     window.addEventListener("keyup", keyupListener);
     window.addEventListener("keydown", keydownListener);
@@ -218,24 +77,9 @@ export const SynthOscillator = () => {
     return () => {
       window.removeEventListener("keyup", keyupListener);
       window.removeEventListener("keydown", keydownListener);
-
-      try {
-        oscRef.current?.stop();
-        osc2Ref.current?.stop();
-        audioRef.current?.close();
-      } catch {}
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [
-    params.delay,
-    params.envelope,
-    params.gain,
-    params.octave,
-    params.reverb,
-    params.waveform,
-    params.filter,
-    params.secondOscOn,
-  ]);
+  }, []);
 
   return <></>;
 };

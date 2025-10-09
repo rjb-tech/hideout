43c6d1709ada0d673bf379464df2f7f0cd472645
Ryne Burden <ryneburden@protonmail.com>
2025-10-08 20:12:33 -0400

    split oscillator into own component and enable synth settings reuse again

--- src/stores/synth.ts
+++ src/stores/synth.ts
@@ -5,29 +5,33 @@ import type { SynthSettings } from "@hideoutTypes/synth";
 
 export const SYNTH_LOCAL_STORAGE_KEY = "synth_settings";
 
+const existingSettings = JSON.parse(
+  window.sessionStorage.getItem(SYNTH_LOCAL_STORAGE_KEY) ?? "{}",
+) as SynthSettings;
+
 export const synthParamsStore: PreinitializedMapStore<SynthSettings> = map({
-  gain: GAIN_MAX,
-  octave: 2,
-  waveform: "sawtooth",
-  envelope: {
+  gain: existingSettings.gain ?? GAIN_MAX,
+  octave: existingSettings.octave ?? 2,
+  waveform: existingSettings.waveform ?? "sawtooth",
+  envelope: existingSettings.envelope ?? {
     attack: 0,
     decay: 15,
     sustain: 0,
     release: 0,
   },
-  delay: {
+  delay: existingSettings.delay ?? {
     time: null,
     feedback: 0, // not working
   },
-  reverb: {
+  reverb: existingSettings.reverb ?? {
     decay: null,
     mix: 0.4,
   },
-  filter: {
+  filter: existingSettings.filter ?? {
     frequency: 20000,
     type: "lowpass",
     q: 0,
     envelopeLink: false,
   },
-  secondOscOn: false,
+  secondOscOn: existingSettings.secondOscOn ?? false,
 });
